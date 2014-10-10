caa93be663f756f399d3235c221b0324782dfda6
shoaibkhan-aspose <shoaib.khan@banckle.com>
2014-10-10 19:30:46 +0500

    Missing Features of Docx4j - v 1.0

---
+++ Missing Features of Docx4j/src/aspose/words/AsposeCloneDoc.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Aspose 2002-2014. All Rights Reserved.
+ */
+
+package aspose.words;
+
+import com.aspose.words.Document;
+import com.aspose.words.SaveFormat;
+
+public class AsposeCloneDoc
+{
+	public static void main(String[] args) throws Exception
+	{
+		Document doc = new Document("data/document.doc");
+		Document clone = doc.deepClone();
+		clone.save("data/AsposeClone.doc", SaveFormat.DOC);
+	}
+}
